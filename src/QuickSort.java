bbc3fd2441252b2f505f1b9ac1b41bc00272e419
Star7-jpg <marcosconde612@gmail.com>
2023-11-13 23:45:39 -0600

    03 Actualización inútil que me dejó en el mismo lugar

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,20 +1,22 @@
 public class QuickSort {
+
     public static void main(String[] args) throws Exception {
         int[] array = {3,1,7,9,6,8,4,2,5};
-        
-        
+        int pivote = array[array.length/2];  //El pivote es el valor en la posición 4 del arreglo
 
-        for (int i = 0; i < array.length/2; i++) {
-            System.out.println(array[i]);
+        System.out.println("Izquierda");
+        for (int i = 0; i < array.length; i++) {  
+            if (array[i] < pivote) {   //Si el valor de la casilla es menor que el pivote, se imprime
+                System.out.print(array[i] + " ");
+            } 
         }
 
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
-
-        for (int j = array.length-1; j > array.length/2; j--) {
-            System.out.println(array[j]);
+        System.out.println();
+        System.out.println("Derecha");
+        for (int j = array.length-1; j > 0; j--) {
+            if (array[j] > pivote) {   
+                System.out.print(array[j] + " ");
+            } 
         }
-
-        // System.out.println(array[array.length-1]);
-        // System.out.println(array[array.length/2]);
     }
 }
